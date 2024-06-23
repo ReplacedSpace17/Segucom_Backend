8431f2768a800c3fbc30f20e69c0f57f5cfc11ba
ReplacedSpace17 <replacedspace17@gmail.com>
2024-06-23 11:22:16 -0600

    bh

---
+++ #Habilitar shh.sh
@@ -0,0 +1,47 @@
+#Habilitar shh
+sudo systemctl status ssh
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+sudo nano /etc/ssh/sshd_config
+sudo ufw allow ssh
+
+ssh user@ip -p 22
+
+#server ssh
+sermex-segu
+S3rs6uc0
+
+
+#wifi
+clave modem:
+HaEkt6hYhY
+
+red:
+53RM3XC5IGTO
+
+#base de dtos:
+segucomm_db
+segucomm_admin
+s3guC0m@7am
+
+
+#ss externo
+ssh sermex-segu@segubackend.com -p 22
+S3rs6uc0
+
+#ss interno
+ssh sermex-segu@192.168.1.71 -p 22
+S3rs6uc0
+
+##actualizar el back
+git pull 
+ sudo systemctl restart backendsegucom.service
+ sudo systemctl stop backendsegucom.service
+ sudo systemctl start backendsegucom.service
+ sudo systemctl status backendsegucom.service
+ sudo systemctl enable backendsegucom.service
+
+
+ #invitados c5
+9f@5i2H&7LQA
