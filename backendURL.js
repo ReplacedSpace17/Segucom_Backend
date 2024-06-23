1aab550033c2fc8609081476706e2cff10c5455a
ReplacedSpace17 <replacedspace17@gmail.com>
2024-06-23 12:52:16 -0600

    chore: Actualizar URL de backend en archivo backendURL.js

--- backendURL.js
+++ backendURL.js
@@ -1,4 +1,4 @@
 //const backendUrl = 'http://192.168.1.76:3000';
 //url del backend para las paginas de mapas y subir imagenes
 
-const backendUrl = 'https://segubackend:3000';
+const backendUrl = 'https://segubackend.com:3000';
