0a61e4cfdc8c596a3bb658c9edb9ac71781626e0
ReplacedSpace17 <replacedspace17@gmail.com>
2024-07-17 11:18:46 -0600

    ok

--- Functions/Maps/Function_elemento.js
+++ Functions/Maps/Function_elemento.js
@@ -278,7 +278,7 @@ async function UpdateUbicacion(req, res, data, Num_tel) {
                         AddElementoFueraZona(req, res, data.ELEMENTO_NUM, data.ELEMENTO_LATITUD, data.ELEMENTO_LONGITUD, 1, data.ELEMENTO_ULTIMALOCAL);
                     } 
                 }
-                
+                res.status(200).send('Ubicación actualizada con éxito');
             }
         });
     } catch (error) {
